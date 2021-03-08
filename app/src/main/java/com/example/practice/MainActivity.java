08fb851fa9e32bb70981c02dfd561631ef1a7a32
Daydeuk <71961156+Daydeuk@users.noreply.github.com>
2021-03-08 22:42:05 +0900

    First Commit

---
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.practice;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+
+    public void onButton1cliked(View v){
+
+    }
+}
---
+++ app/src/main/java/com/example/practice/cellFragment.java
@@ -0,0 +1,27 @@
+package com.example.practice.;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class cellFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_cell, container, false);
+        RecyclerView recyclerView = v.findViewById(R.id.cellRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        RecyclerAdapter adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        return v;
+    }
+}
